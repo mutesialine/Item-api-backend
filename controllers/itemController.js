69bb0de8a6bc20a9e7127247e0c031666e8a8718
mutesialine <ktesialine@gmail.com>
2023-08-24 22:50:42 +0200

    refactor:response status code

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -8,10 +8,11 @@ const createPostItems = (req, res)=>{
        })
        item.save()
        .then(result =>{
-        res.json(result)
+        res.status(201).json(result)
        })
        .catch (err =>{
         console.log(err)
+        res.status(500).json({ error: 'Internal Server Error' });
        })
     }
 
