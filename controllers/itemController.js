1b4ff4be7b2ccc338fb4e0b9eab9ac5554b4ca18
mutesialine <ktesialine@gmail.com>
2023-08-24 22:58:43 +0200

    add status code 200 and 404

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -31,10 +31,11 @@ const getSingleItem = (req, res)=>{
     const id = req.params.id
     Item.findById(id)
     .then(result =>{
-        res.json(result)
+        res.status(200).json(result)
     })
     .catch(err =>{
         console.log(err)
+        res.status(404).json({ error: 'Item not found' })
         ; 
     })
 }
